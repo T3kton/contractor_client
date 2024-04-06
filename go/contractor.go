74dfe0613c38c303b55f17e217feb0c4242976ac
Peter Howe <pnhowe@gmail.com>
2024-04-06 15:01:57 -0600

    reworking where the auto generated code goes

--- go/contractor.go
+++ go/contractor.go
@@ -3,34 +3,24 @@ package contractor
 import (
 	"context"
 	"log/slog"
-
-	ContractorAutoGen "github.com/t3kton/contractor_client/go/autogen"
 )
-
-// Contractor struct
-type Contractor struct {
-	*ContractorAutoGen.Contractor
-}
 
 // NewContractor creates a new Contractor and logs it in with the username and password
 func NewContractor(ctx context.Context, log *slog.Logger, host string, proxy string, username string, password string) (*Contractor, error) {
-	c, err := ContractorAutoGen.NewContractor(ctx, log, host, proxy)
+	c, err := NewContractorInt(ctx, log, host, proxy)
 	if err != nil {
 		return nil, err
 	}
-
-	c2 := Contractor{}
-	c2.Contractor = c
 
 	token, err := c.AuthUserCallLogin(ctx, username, password)
 	if err != nil {
 		return nil, err
 	}
 
-	c2.SetHeader("Auth-Id", username)
-	c2.SetHeader("Auth-Token", token)
+	c.SetHeader("Auth-Id", username)
+	c.SetHeader("Auth-Token", token)
 
-	return &c2, nil
+	return c, nil
 }
 
 // Logout calles logout and removes the auth headers
